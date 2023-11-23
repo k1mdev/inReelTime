86d53ab29925d168d2354c078018dbf2cdf94a75
k0nem <eamonkon@gmail.com>
2023-11-23 12:37:52 -0500

    Initial Delete Catch page

--- frontend/src/pages/DeleteCatch.jsx
+++ frontend/src/pages/DeleteCatch.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { React, useState } from 'react'
+import axios from 'Axios'
+import { useNavigate, useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/spinner'
 
 const DeleteCatch = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {id} = useParams();
+
+  const handleDeleteCatch = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/catches/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error occurred. Please check console.');
+        console.log(error);
+      });
+  }
+
   return (
-    <div>DeleteCatch</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Delete Catch</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <h3>Are you sure you want to delete this catch?</h3>
+        <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteCatch}
+        >
+          Yes
+        </button>
+      </div>
+    </div>
   )
 }
 
