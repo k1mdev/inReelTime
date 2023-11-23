82738dfc313166390d29e6728eee6cc0ab44cba0
k0nem <eamonkon@gmail.com>
2023-11-23 14:25:54 -0500

    Implemented table and card views

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { BsInfoCircle } from 'react-icons/bs'
 import { MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md'
-
+import CatchTable from '../components/home/CatchTable'
+import CatchCard from '../components/home/CatchCard'
 
 const Home = () => {
   const [catches, setCatches] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState('table');
   useEffect(() => {
     setLoading(true);
     axios
@@ -25,6 +27,20 @@ const Home = () => {
   }, []);
   return (
     <div className='p-4'>
+      <div className='flex justify-center items-center gap-x-4'>
+        <button
+          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg '
+          onClick={() => setShowType('table')}
+        >
+          Table
+        </button>
+        <button
+          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg '
+          onClick={() => setShowType('card')}
+        >
+          Card
+        </button>
+      </div>
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Catch List</h1>
         <Link to='/catches/create'>
@@ -33,58 +49,10 @@ const Home = () => {
       </div>
       {loading ? (
         <Spinner />
+      ) : showType == 'table' ? (
+        <CatchTable catches={catches} />
       ) : (
-        <table className='w-full border-separate border-spacing-2'>
-          <thead>
-            <tr>
-              <th className='border border-slate-600 rounded-md'>No</th>
-              <th className='border border-slate-600 rounded-md'>Species</th>
-              <th className='border border-slate-600 rounded-md'>Date</th>
-              {/* max-md:hidden hides column in mobile and tablet vps  */}
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Length</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Lure</th>
-              <th className='border border-slate-600 rounded-md'>Operations</th>
-
-            </tr>
-          </thead>
-          
-          <tbody>
-            {catches.map((catchLog, index) => (
-              <tr key={catchLog.id} className='h-8'>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  {index + 1}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  {catchLog.species}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  {catchLog.date}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                  {catchLog.length}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-                  {catchLog.lure}
-                </td>
-                <td className='border border-slate-700 rounded-md text-center'>
-                  <div className='flex justify-center gap-x-4'>
-                    <Link to={`/catches/details/${catchLog._id}`}>
-                      <BsInfoCircle className='text-2xl text-green-800' />
-                    </Link>
-                    <Link to={`/catches/edit/${catchLog._id}`}>
-                      <AiOutlineEdit className='text-2xl text-yellow-800' />
-                    </Link>
-                    <Link to={`/catches/delete/${catchLog._id}`}>
-                      <MdOutlineDelete className='text-2xl text-red-800' />
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-
-        </table>
-
+        <CatchCard catches={catches} />
       )}
     
     </div>
