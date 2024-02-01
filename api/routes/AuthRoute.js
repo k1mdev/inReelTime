c326d2044946e14aa8a6f427d6d01ddcb50ebfbd
k0nem <eamonkon@gmail.com>
2024-01-31 23:05:33 -0500

    Export error fix

--- api/routes/AuthRoute.js
+++ api/routes/AuthRoute.js
@@ -2,10 +2,10 @@ import { Signup, Login } from "../controllers/AuthController.js";
 import express from "express";
 import { userVerification } from "../middlewares/AuthMiddleware.js";
 
-export const authRouter = express.Router();
+export const authRoute = express.Router();
 
-authRouter.post("/signup", Signup);
-authRouter.post("/login", Login);
-authRouter.post("/", userVerification)
+authRoute.post("/signup", Signup);
+authRoute.post("/login", Login);
+authRoute.post("/", userVerification)
 
 
